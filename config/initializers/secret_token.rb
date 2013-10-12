61dcc927dc6fb8eed541ea5b5e12d74499599b4a
Ryan Hennig <ryhennig@ebay.com>
2013-10-11 22:12:40 -0700

    ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodRescueReceipt::Application.config.secret_token = '9d77b5bb5ccf4b2be25ba9ee9e8dd765ab3bb9754008042621b02e8481e1f7859031bc3d75d7638c17240cdfba6cea55ba4b37b5be8ac3dd223e79c963fbbe99'
